498505eda2d27aa8aacfe08278010f94ac9d1559
hardikbaweja <64698268+hardikbaweja@users.noreply.github.com>
2020-05-23 16:51:54 +0530

    Add files via upload

---
+++ SpeakGodBye1.js
@@ -0,0 +1,13 @@
+(function() {
+
+var byeSpeaker = {};
+var speakword = "Good Bye";
+byeSpeaker.speak = function(name) {
+	 console.log(speakWord + " " + name);
+  window.byeSpeaker=byeSpeaker;
+}
+
+
+	
+})();
+
---
+++ SpeakHello1.js
@@ -0,0 +1,22 @@
+
+(function(window) {
+
+var helloSpeaker = {};
+
+
+var speakWord = "Hello";
+
+
+helloSpeaker.speak=function (name) {
+  console.log(speakWord + " " + name);
+}
+window.helloSpeaker=helloSpeaker;
+
+
+
+
+
+
+
+})(window);
+
